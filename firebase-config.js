2ffb5f30aeb04f52ee8d10711aba9db07207cd46
Danny <shallytbiznes@gmail.com>
2026-07-27 12:29:18 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-// Import Firebase
+// Firebase v11
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
@@ -18,4 +18,9 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+
+try {
+  getAnalytics(app);
+} catch (e) {
+  // Analytics nie działa na localhost i czasami na GitHub Pages
+}
