052102857d1beb0fe83fd032ce130b3530b124c9
Danny <shallytbiznes@gmail.com>
2026-07-28 12:51:57 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getAnalytics, isSupported } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCffBZPf5HdnsZW_XmRnnvMx9IH8K-Gynw",
-  authDomain: "subrodzinny.firebaseapp.com",
-  projectId: "subrodzinny",
-  storageBucket: "subrodzinny.firebasestorage.app",
-  messagingSenderId: "101440164805",
-  appId: "1:101440164805:web:756b43a42bea39da90f1ac",
-  measurementId: "G-KFB5VZL9F3"
+  apiKey: 'AIzaSyCffBZPf5HdnsZW_XmRnnvMx9IH8K-Gynw',
+  authDomain: 'subrodzinny.firebaseapp.com',
+  projectId: 'subrodzinny',
+  storageBucket: 'subrodzinny.firebasestorage.app',
+  messagingSenderId: '101440164805',
+  appId: '1:101440164805:web:756b43a42bea39da90f1ac',
+  measurementId: 'G-KFB5VZL9F3'
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
-isSupported()
-  .then((supported) => {
-    if (supported) getAnalytics(app);
-  })
-  .catch((error) => console.warn("Analytics niedostępne:", error));
